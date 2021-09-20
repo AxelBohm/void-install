9f5cbe17c05da0c6b68053a7fcd000156a53642d
AxelBohm <axel.boehm@ymail.com>
2021-09-20 17:31:46 +0000

    also automate most important folder structure

---
+++ set_up_documents.sh
@@ -0,0 +1,19 @@
+
+varsmooth-general-smooth-composition
+
+
+
+mkdir academia
+
+git clone git@github.com:AxelBohm/bibliography.git academia/bibliography
+
+mkdir academia/courses
+git clone git@github.com:AxelBohm/optimization-for-DS-lecture.git academia/courses/optimization-for-DS-lecture
+
+
+mkdir academia/research_projects
+cd academia/research_projects
+git clone git@github.com:AxelBohm/weak-minty-OGDA.git 
+git clone git@github.com:AxelBohm/varsmooth-general-smooth-composition.git 
+git clone git@github.com:AxelBohm/accidential-saddle.git
+git@github.com:AxelBohm/varsmooth-general-smooth-composition.git
